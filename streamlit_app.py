3d6b23128b794d53a622ed5ba4142bd10340e0a9
andrewgillespie <3072699+andrewgillespie@users.noreply.github.com>
2026-07-17 14:24:01 +0100

    Adding call to smoothiefroot api

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,14 @@ import streamlit as st
 import os
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customise Your Smoothie :cup_with_straw:") 
 st.write("Choose the fruits you want in your custom smoothie!")
+
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
