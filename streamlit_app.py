911812774734a3b136dcbf44cd6dcecd6688c50e
andrewgillespie <3072699+andrewgillespie@users.noreply.github.com>
2026-07-17 14:40:06 +0100

    Refactor smoothie ordering to include nutrition info
    Updated the smoothie order process to fetch nutrition information for each selected fruit and display it in the app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,20 +18,15 @@ st.write('The name on your smoothie will be:',  name_on_order)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 ingredients_list = st.multiselect("Choose up to 5 ingredients:", my_dataframe, max_selections=5)
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
-    st.write(ingredients_string)
+        st.subheader(fruit_chosen + ' Nutrition Information')
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-                    values ('""" + ingredients_string + """', '""" + name_on_order + "')"""
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-#    st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
