d7bd65ffb3430c055885b53ec3274c0290398d22
andrewgillespie <3072699+andrewgillespie@users.noreply.github.com>
2026-07-17 16:09:16 +0100

    Refactor smoothie order submission logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,6 @@ st.write("The name on your smoothie will be:",  name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 pd_df = my_dataframe.to_pandas()
-st.dataframe(data=my_dataframe, width='stretch')
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 
@@ -33,9 +32,8 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + {search_on})
         sf_df = st.dataframe(data=smoothiefroot_response.json(), width='stretch')
-#    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-#                    values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-    time_to_insert = st.button('Submit Order')
+
+        time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
