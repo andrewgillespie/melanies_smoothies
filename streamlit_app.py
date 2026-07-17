7de93036424369179f470a26755e7fc319af5354
andrewgillespie <3072699+andrewgillespie@users.noreply.github.com>
2026-07-17 14:57:04 +0100

    Change selected column in fruit options dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,10 @@ session = cnx.session()
 name_on_order = st.text_input("Name on smoothie:")
 st.write('The name on your smoothie will be:',  name_on_order) 
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, width='stretch')
+st.stop()
+
 ingredients_list = st.multiselect("Choose up to 5 ingredients:", my_dataframe, max_selections=5)
 
 if ingredients_list:
